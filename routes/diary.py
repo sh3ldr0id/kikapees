d54547f0c209603302b3e9bc26b3f42425bdfc20
Sheldon Sinu <sh3ldr0id@gmail.com>
2024-02-08 15:27:26 +0530

    added diary

--- routes/diary.py
+++ routes/diary.py
@@ -7,8 +7,11 @@ blueprint = Blueprint(
     __name__
 )
 
-@blueprint.route('/view/<date>/<user>')
+@blueprint.route('/view/<custom_date:date>/<user>')
 def view(date, user):
+    if not date:
+        return redirect("/404")
+
     token = session.get("token")
 
     authorized = db.reference(f"auth/{token}").get()
@@ -17,11 +20,17 @@ def view(date, user):
         return redirect("/")
     
     page = db.reference(f"diary/{date}/{user}").get()
-
-    return render_template("diary/page.html", date=date, page=page)
     
-@blueprint.route('/write/<date>', methods=["GET", "POST"])
+    other = "Kunji" if user == "kunja" else "Kunja"
+    
+    return render_template("diary/page.html", date=date.strftime('%A, %d %B %Y'), other=other, page=page)
+    
+@blueprint.route('/write/<custom_date:date>', methods=["GET", "POST"])
 def write(date):
+    if not date:
+        print(date)
+        return redirect("/404")
+
     token = session.get("token")
 
     authorized = db.reference(f"auth/{token}").get()
@@ -37,16 +46,10 @@ def write(date):
         return render_template("diary/write.html", page=page)
     
     elif request.method == "POST":
-        day = request.form["day"]
         content = request.form["content"]
     
         page = db.reference(f"diary/{date}").update({
-            user: {
-                "day": day,
-                "content": content
-            }
+            user: content
         })
-
-        print(date, content)
 
         return redirect(f"/diary/view/{date}/{user}")
--- run.py
+++ run.py
@@ -7,14 +7,30 @@ from routes.bucket import blueprint as bucket_blueprint
 from routes.memories import blueprint as memories_blueprint
 from routes.diary import blueprint as diary_blueprint
 
+from werkzeug.routing import BaseConverter
+from datetime import datetime
+
+class CustomDateConverter(BaseConverter):
+    def to_python(self, value):
+        try:
+            return datetime.strptime(value, '%Y-%m-%d').date()
+        
+        except ValueError:
+            return None
+
+    def to_url(self, value):
+        return value.strftime('%Y-%m-%d')
+
 app = Flask(__name__)
 app.secret_key = 'SECRET_KEY'
 
+app.url_map.converters['custom_date'] = CustomDateConverter
+
 init_db()
 
-@app.errorhandler(404)
-def not_found(e):
-    return "Invalid/Broken URL"
+# @app.errorhandler(404)
+# def not_found(e):
+#     return "Invalid/Broken URL"
 
 @app.errorhandler(500)
 def not_found(e):
