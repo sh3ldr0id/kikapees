30c32123d4e931d8531d81d74f5f1271cce08ddc
Sheldon Sinu <sh3ldr0id@gmail.com>
2024-02-09 23:20:23 +0530

    added navbar to home

--- routes/diary.py
+++ routes/diary.py
@@ -25,7 +25,7 @@ def home():
     if not authorized:
         return redirect("/")
 
-    return render_template("diary/home.html", date=datetime.now().strftime('%Y-%m-%d'), other=other(authorized['user']))    
+    return render_template("diary/home.html", date=datetime.now().strftime('%Y-%m-%d'), other=other(authorized))    
 
 @blueprint.route('/view/<custom_date:date>/<user>')
 def view(date, user):
@@ -41,7 +41,7 @@ def view(date, user):
     
     page = db.reference(f"diary/{date}/{user}").get()
         
-    return render_template("diary/page.html", date=date.strftime('%A, %d %B %Y'), user=authorized['user'].capitalize(), page=page)
+    return render_template("diary/page.html", date=date.strftime('%A, %d %B %Y'), user=authorized.capitalize(), page=page)
     
 @blueprint.route('/write/<custom_date:date>', methods=["GET", "POST"])
 def write(date):
@@ -61,7 +61,7 @@ def write(date):
     if request.method == "GET":
         page = db.reference(f"diary/{date}/{user}").get()
 
-        return render_template("diary/write.html", date=date.strftime('%A, %d %B %Y'), other=other(authorized['user']).capitalize(), page=page)
+        return render_template("diary/write.html", date=date.strftime('%A, %d %B %Y'), other=other(authorized).capitalize(), page=page)
     
     elif request.method == "POST":
         content = request.form["content"]
