5cd9e6b3edb56d8efa65b4f3a91bbd9508ee7450
Sheldon Sinu <sh3ldr0id@gmail.com>
2024-02-18 21:07:28 +0530

    changed thumbnail in view

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import create_app
+from os.path import exists
+from os import mkdir
 
 app = create_app()
+
+if not exists("temp/"):
+    mkdir("temp/")
 
 if __name__ == "__main__":
     app.run()
