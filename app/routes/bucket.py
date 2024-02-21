e576c5e877299e483f8c396e594bb6fe6e34ef4f
Sheldon Sinu <sh3ldr0id@gmail.com>
2024-02-21 23:21:09 +0530

    added reel

--- app/routes/bucket.py
+++ app/routes/bucket.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request, redirect, render_template, session
-from firebase_admin import db
+from firebase_admin import db, storage
 from datetime import datetime
+from requests import get
+from uuid import uuid4
+from cv2 import VideoCapture, CAP_PROP_FRAME_COUNT, CAP_PROP_POS_FRAMES, imwrite
 
 blueprint = Blueprint(
     'bucket', 
@@ -63,14 +66,69 @@ def create():
     elif request.method == "POST":
         title = request.form["title"]
         description = request.form["description"]
+        reel = request.form["reel"]
 
         by = authorized["user"]
+
+        filename = None
+
+        if reel:
+            try:
+                link = get(
+                    f"https://instagram-videos.vercel.app/api/video?url={reel}"
+                ).json()["data"]["videoUrl"]
+
+                filename = f"temp/{str(uuid4())}.mp4"
+
+                with open(filename, 'wb') as file:
+                    file.write(
+                        get(link).content
+                    )
+            
+            except:
+                return "Invalid Reel Link"
+            
+        url = None,
+        thumbnail = None
+
+        if filename:
+            bucket = storage.bucket("kikapees.appspot.com")
+
+            blob = bucket.blob(f"bucket/files")
+            blob.upload_from_filename(filename)
+            blob.make_public()
+
+            url = blob.public_url
+
+            cap = VideoCapture(filename)
+
+            skip_frames = round(
+                int(
+                    cap.get(CAP_PROP_FRAME_COUNT)
+                ) / 2
+            )
+
+            cap.set(CAP_PROP_POS_FRAMES, skip_frames)
+
+            _, frame = cap.read()
+
+            imwrite(f"{filename.split('.')[0]}.png", frame)
+
+            cap.release()
+
+            blob = bucket.blob(f"bucket/thumbnails")
+            blob.upload_from_filename(f"{filename.split('.')[0]}.png")
+            blob.make_public()
+
+            thumbnail = blob.public_url
 
         bucket = db.reference("bucket")
 
         bucket.push({
             "title": title,
             "description": description,
+            "url": url,
+            "thumbnail": thumbnail,
             "date": datetime.now().strftime("%Y-%m-%d %H:%M"),
             "by": by
         })
